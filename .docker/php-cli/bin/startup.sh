53459cc66dbfb3878b7c7af68884adabb202747c
Haseeb Memon <memon.haseeb@hotmail.com>
2019-09-18 11:07:45 +0400

    magento installer updated

--- .docker/php-cli/bin/startup.sh
+++ .docker/php-cli/bin/startup.sh
@@ -1,11 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 ########################################################################################################################
 # Composer Magento Auth Setup
 ########################################################################################################################
 echo "Setup Magento Auth For Composer";
 composer global config http-basic.repo.magento.com $PUBLIC_KEY $PRIVATE_KEY
 ########################################################################################################################
-echo "Project";
 cd $PROJECT_ROOT
 ########################################################################################################################
 if [ -f ./app/etc/config.php ] || [ -f ./app/etc/env.php ]; then
@@ -14,8 +13,71 @@ if [ -f ./app/etc/config.php ] || [ -f ./app/etc/env.php ]; then
 fi
 ########################################################################################################################
 echo "Delete default index files";
-rm -rf * && rm -rf .DS_Store
+rm -rf * && rm -rf .*
 ########################################################################################################################
 echo "Download Magento...";
-composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition $PROJECT_ROOT ;
+if [ -z $MAGENTO_VERSION ]; then
+    composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$MAGENTO_VERSION $PROJECT_ROOT ;
+else
+    composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition $PROJECT_ROOT ;
+fi
+########################################################################################################################
+echo "Fixing File Permissions...";
+chmod u+w -R ./var ./vendor ./pub/static ./pub/media ./app/etc
+chmod u+x ./bin/magento
+########################################################################################################################
+echo "Installing Magento"
+php bin/magento setup:install \
+--admin-firstname=$MAGENTO_ADMIN_FIRSTNAME \
+--admin-lastname=$MAGENTO_ADMIN_LASTNAME \
+--admin-email=$MAGENTO_ADMIN_EMAIL \
+--admin-user=$MAGENTO_ADMIN_USERNAME \
+--admin-password='admin123' \
+--base-url=$MAGENTO_BASE_URL \
+--base-url-secure=$MAGENTO_SECURE_BASE_URL \
+--backend-frontname=$MAGENTO_BACKEND_FRONTNAME \
+--db-host=$MAGENTO_DB_HOST \
+--db-name=$MAGENTO_DB_NAME \
+--db-user=$MAGENTO_DB_USER \
+--db-password=$MAGENTO_DB_PASSWORD \
+--use-rewrites=1 \
+--language=$MAGENTO_LANGUAGE \
+--currency=$MAGENTO_DEFAULT_CURRENCY \
+--timezone=$MAGENTO_TIMEZONE \
+--use-secure-admin=1 \
+--admin-use-security-key=1 \
+--session-save=files \
+--use-sample-data;
+########################################################################################################################
+if [ "$MAGENTO_USE_SAMPLE_DATA" == "true" ]; then
+    echo "Install Sample Data";
+    cp /var/www/.composer/auth.json /var/www/html/var/composer_home/auth.json;
+    php -f ./bin/magento sampledata:deploy;
+    php -f ./bin/magento setup:upgrade;
+fi
+########################################################################################################################
+echo "Configure Redis"
+php bin/magento setup:config:set --cache-backend=redis --cache-backend-redis-server=redis-cache     --cache-backend-redis-port=6379 --cache-backend-redis-db=0;
+php bin/magento setup:config:set --page-cache=redis    --page-cache-redis-server=redis-page-cache   --page-cache-redis-port=6379    --page-cache-redis-db=0;
+echo "Y" | php bin/magento setup:config:set --session-save=redis  --session-save-redis-host=redis-session      --session-save-redis-port=6379 --session-save-redis-log-level=3 --session-save-redis-db=0;
+########################################################################################################################
+echo "Configure ElasticSearch"
+#php bin/magento cache:flush;
+#php bin/magento indexer:reindex catalogsearch_fulltext
+#php bin/magento indexer:reindex
+########################################################################################################################
+echo "Configure Magento"
+php bin/magento maintenance:enable;
+php bin/magento setup:upgrade;
+php bin/magento setup:di:compile;
+php bin/magento setup:static-content:deploy -f;
+php bin/magento indexer:reindex;
+php bin/magento indexer:set-mode schedule;
+php bin/magento cache:flush;
+php bin/magento maintenance:disable;
+########################################################################################################################
+echo "Optimize Magento"
+#rm -rf var/cache var/page_cache var/generation var/di generated/*
+#rm -rf generated/metadata/* generated/code/*
+#php bin/magento deploy:mode:set production
 ########################################################################################################################
